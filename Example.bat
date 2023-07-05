5152e3a6dc2728d8591ab648c2d8d5e247bfb00b
HenrikDueholm <henrikdue@yahoo.dk>
2023-07-05 21:45:41 +0200

    First commit after build support was added.

---
+++ Example.bat
@@ -0,0 +1,2 @@
+MultiClone https://github.com/HenrikDueholm/LV32.2020..PPL.HDH.Driver.DMM;https://github.com/HenrikDueholm/LV32.2020..PPL.ClassLoader --path "C:\Current projects\Python\Test" --version-action 1 --force True --depth 1
+pause
---
+++ README.md
@@ -0,0 +1,2 @@
+# App_MultiClone
+Python app for multi cloning repositories
