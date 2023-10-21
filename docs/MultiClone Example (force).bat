ff58756d3664feb1b1b93d2a85a98fa1b16f4d8a
HenrikDueholm <henrikdue@yahoo.dk>
2023-10-21 22:55:41 +0200

    Improved execution examples (now fully path independent). Fixed issue with build not working if target folder did not exists. Updated version.

--- docs/MultiClone Example (force).bat
+++ docs/MultiClone Example (force).bat
@@ -1,8 +1,27 @@
 @echo off
-ECHO Running MultiClone example cloning 2 target repositories with 2 dependent repositories from https://github.com/HenrikDueholm/.
+
+setlocal enabledelayedexpansion
+set "currentFilePath=%~dp0"
+pushd "%currentFilePath%\..\.."
+set "rootFolderPath=!CD!"
+set "targetPath=!rootFolderPath!\Test"
+popd
+cd..
+
+ECHO.
+ECHO Run MultiClone example:
+ECHO - Execute from %CD%
+ECHO - Clone 2 target repositories with 2 dependent repositories from https://github.com/HenrikDueholm/
+ECHO - Clone to %targetPath%
+ECHO - Clone with force option enabled (repositories will be delted and re-cloned)
 ECHO.
 ECHO _______________________________________________________________________
 ECHO.
-cd..
-"MultiClone.exe" https://github.com/HenrikDueholm/LV32.2020..PPL.HDH.Driver.DMM;https://github.com/HenrikDueholm/LV32.2020..PPL.ClassLoader --path "C:\Current projects\Python\Test" --version-action 1 --force --depth 1
+
+"MultiClone.exe" https://github.com/HenrikDueholm/LV32.2020..PPL.HDH.Driver.DMM;https://github.com/HenrikDueholm/LV32.2020..PPL.ClassLoader --path "%targetPath%" --version-action 1 --force --depth 1
+
+ECHO _______________________________________________________________________
+ECHO.
+
+endlocal
 pause
--- docs/MultiClone Example (no force).bat
+++ docs/MultiClone Example (no force).bat
@@ -1,8 +1,27 @@
 @echo off
-ECHO Running MultiClone example cloning 2 target repositories with 2 dependent repositories from https://github.com/HenrikDueholm/.
+
+setlocal enabledelayedexpansion
+set "currentFilePath=%~dp0"
+pushd "%currentFilePath%\..\.."
+set "rootFolderPath=!CD!"
+set "targetPath=!rootFolderPath!\Test"
+popd
+cd..
+
+ECHO.
+ECHO Run MultiClone example:
+ECHO - Execute from %CD%
+ECHO - Clone 2 target repositories with 2 dependent repositories from https://github.com/HenrikDueholm/
+ECHO - Clone to %targetPath%
+ECHO - Clone without force option enabled (previously cloned repositories will be skipped)
 ECHO.
 ECHO _______________________________________________________________________
 ECHO.
-cd..
-"MultiClone.exe" https://github.com/HenrikDueholm/LV32.2020..PPL.HDH.Driver.DMM;https://github.com/HenrikDueholm/LV32.2020..PPL.ClassLoader --path "C:\Current projects\Python\Test" --version-action 1 --depth 1
+
+"MultiClone.exe" https://github.com/HenrikDueholm/LV32.2020..PPL.HDH.Driver.DMM;https://github.com/HenrikDueholm/LV32.2020..PPL.ClassLoader --path "%targetPath%" --version-action 1 --depth 1
+
+ECHO _______________________________________________________________________
+ECHO.
+
+endlocal
 pause
